b8a2a31152f438f6f61858d2539af0dce6d6cd26
f0rnell <jformar967@g.educaand.es>
2022-02-11 14:53:45 +0100

    Commit 11/02/2022 Proyecto HLC

--- controlador.js
+++ controlador.js
@@ -1,24 +1,28 @@
 $(function () {
+    // TEST cache
+    var n = Math.floor(Math.random() * 11);
+    var k = Math.floor(Math.random() * 1000000);
+    var uniqid = String.fromCharCode(n) + k;
 
-    $('#cabecera').load("partials/cabecera.html");
+    $('#cabecera').load("partials/cabecera.html?"+ uniqid);
 
-    $('#pie').load("partials/pie.html");
+    $('#pie').load("partials/pie.html?"+ uniqid);
 
     setTimeout(botones,1000);
 
     function botones(){
-        $('#cuerpo').load("paginas/paginaPrincipal.html");
+        $('#cuerpo').load("paginas/paginaPrincipal.html?"+ uniqid);
 
         $('#principal').click(()=>{
-            $('#cuerpo').load("paginas/paginaPrincipal.html")       
+            $('#cuerpo').load("paginas/paginaPrincipal.html?"+ uniqid)       
         });
     
         $('#menus').click(()=>{
-            $('#cuerpo').load("paginas/paginaMenus.html")
+            $('#cuerpo').load("paginas/paginaMenus.html?"+ uniqid)
         });
     
         $('#reservas').click(()=>{
-            $('#cuerpo').load("paginas/paginaReservas.html")
+            $('#cuerpo').load("paginas/paginaReservas.html?"+ uniqid)
         });
     }
     
