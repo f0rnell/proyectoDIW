f9b5872e86945728118663f89c49ccb411df0d6a
f0rnell <jformar967@g.educaand.es>
2022-03-07 14:46:10 +0100

    Commit 07/03/2022 Proyecto DIW

--- controlador.js
+++ controlador.js
@@ -10,6 +10,11 @@ $(function () {
 
     setTimeout(botones,1000);
 
+    setTimeout(popover,1000);
+    // var popover = new bootstrap.Popover(document.querySelector('.example-popover'), {
+    //     container: 'body'
+    // })
+    
     function botones(){
         $('#cuerpo').load("paginas/paginaPrincipal.html?"+ uniqid);
 
@@ -27,7 +32,18 @@ $(function () {
 
         $('#alergenos').click(()=>{
             $('#cuerpo').load("paginas/paginaAlergenos.html?"+ uniqid)
+            setTimeout(popover,1000);
+        });
+        $('#pedidos').click(()=>{
+            $('#cuerpo').load("paginas/paginaPedidos.html?"+ uniqid)
+            setTimeout(popover,1000);
         });
     }
     
+    function popover(){
+        var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+        var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+            return new bootstrap.Popover(popoverTriggerEl)
+        })
+    }
 });
